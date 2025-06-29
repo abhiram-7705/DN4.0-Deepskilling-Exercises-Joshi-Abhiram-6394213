ea591c50705d7d3da15c04b4e332842a6429c02d
abhiram-7705 <147041608+abhiram-7705@users.noreply.github.com>
2025-06-29 21:05:01 +0530

    Add files via upload

---
+++ Week2_TDD_JUnit5_Mockito/JUnit_BasicTestingExercises/Exercise1/JUnitEx1/src/test/java/com/cts/JUnitEx1/TestExample1.java
@@ -0,0 +1,16 @@
+package com.cts.JUnitEx1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestExample1 {
+
+	@Test
+	public void stringTest() {
+		
+		assertEquals("example 1","example 1");
+		
+	}
+
+}
