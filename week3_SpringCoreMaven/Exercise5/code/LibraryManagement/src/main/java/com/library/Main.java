53f8273ab42f66bf57e4073edb4943846ad2f564
abhiram-7705 <147041608+abhiram-7705@users.noreply.github.com>
2025-07-06 22:15:59 +0530

    Add files via upload

---
+++ week3_SpringCoreMaven/Exercise5/code/LibraryManagement/src/main/java/com/library/Main.java
@@ -0,0 +1,17 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		 BookService service=context.getBean("bookService",BookService.class);
+		 service.addBook("java spring web mvc");
+	}
+
+}
---
+++ week3_SpringCoreMaven/Exercise5/code/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+public class BookRepository {
+		
+	public void save(String name)
+	{
+		System.out.println("book added  : "+name);
+	}
+	
+}
