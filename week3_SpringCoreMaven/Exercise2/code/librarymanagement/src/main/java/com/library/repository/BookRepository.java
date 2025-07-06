c88e53a9b8dd106070f278552cb20ff61f5c8500
abhiram-7705 <147041608+abhiram-7705@users.noreply.github.com>
2025-07-06 19:29:04 +0530

    Add files via upload

---
+++ week3_SpringCoreMaven/Exercise2/code/librarymanagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+public class BookRepository {
+	
+	public void save(String name)
+	{
+		System.out.println("saving a book into library with name :"+name);
+	}
+	
+}
