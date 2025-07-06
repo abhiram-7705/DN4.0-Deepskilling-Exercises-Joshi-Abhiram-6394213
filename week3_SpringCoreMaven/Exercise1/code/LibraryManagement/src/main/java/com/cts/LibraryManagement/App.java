72f86d67b31c6e0b1df089ddb6c26ff9b2fd2f6a
abhiram-7705 <147041608+abhiram-7705@users.noreply.github.com>
2025-07-06 19:15:43 +0530

    Add files via upload

---
+++ week3_SpringCoreMaven/Exercise1/code/LibraryManagement/src/main/java/com/cts/LibraryManagement/App.java
@@ -0,0 +1,22 @@
+package com.cts.LibraryManagement;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        BookService service=context.getBean("bookService",BookService.class);
+        service.saveBook("java spring framework");
+    }
+}
