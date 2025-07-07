404b9637de73593f44345f32be7940bac795f55e
abhiram-7705 <147041608+abhiram-7705@users.noreply.github.com>
2025-07-07 18:00:25 +0530

    Add files via upload

---
+++ week3_SpringCoreMaven/Exercise8/code/LibraryManagement/src/main/java/com/library/service/LibraryService.java
@@ -0,0 +1,13 @@
+package com.library.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibraryService {
+
+	public void method()
+	{
+		System.out.println("service method!!");
+	}
+	
+}
