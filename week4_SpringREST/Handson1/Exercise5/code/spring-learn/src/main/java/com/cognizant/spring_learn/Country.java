3bb246fef6d6cee1234356a5de12d59199ca3742
abhiram-7705 <147041608+abhiram-7705@users.noreply.github.com>
2025-07-15 22:07:44 +0530

    Add files via upload

---
+++ week4_SpringREST/Handson1/Exercise5/code/spring-learn/src/main/java/com/cognizant/spring_learn/Country.java
@@ -0,0 +1,35 @@
+package com.cognizant.spring_learn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Country {
+	
+	private static final Logger LOGGER=LoggerFactory.getLogger(Country.class);
+	
+	private String code;
+	private String name;
+	public Country() {
+		LOGGER.debug("inside the constructor of Country class");
+	}
+	public String getCode() {
+		LOGGER.debug("inside getter of code");
+		return code;
+	}
+	public void setCode(String code) {
+		LOGGER.debug("inside setter of code");
+		this.code = code;
+	}
+	public String getName() {
+		LOGGER.debug("inside getter of name");
+		return name;
+	}
+	public void setName(String name) {
+		LOGGER.debug("inside setter of name");
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Country [code=" + code + ", name=" + name + "]";
+	}
+}
