636cf722b6e5aa3e1d0e4d30aeb8803364608075
abhiram-7705 <147041608+abhiram-7705@users.noreply.github.com>
2025-07-16 14:09:06 +0530

    Add files via upload

---
+++ week4_SpringREST/Handson2/Exercise5/code/spring-learn/src/main/java/com/cognizant/spring_learn/service/CountryService.java
@@ -0,0 +1,31 @@
+package com.cognizant.spring_learn.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.spring_learn.model.Country;
+import com.cognizant.spring_learn.service.exception.CountryNotFoundException;
+
+
+
+@Service
+public class CountryService {
+	
+	public Country getCountry(String code) throws CountryNotFoundException
+	{
+		ApplicationContext context=new ClassPathXmlApplicationContext("country.xml");
+		List<Country> countries=context.getBean("countries",ArrayList.class);
+		
+		for(Country c: countries)
+		{
+			if(c.getCode().equalsIgnoreCase(code))
+				return c;
+		}
+		throw new CountryNotFoundException();
+	}
+
+}
