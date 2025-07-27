427c0bf97d085fdbfa18d85d3472267c6e997de8
abhiram-7705 <147041608+abhiram-7705@users.noreply.github.com>
2025-07-28 00:44:56 +0530

    Add files via upload

---
+++ week6&7_ReactJS/ReactJS-HOL_3/code/scorecalculatorapp/src/App.js
@@ -0,0 +1,15 @@
+import { CalculateScore } from "./components/CalculatorScore";
+
+function App()
+{
+  return(
+    <div>
+      <CalculateScore Name={"abhiram"}
+      School={"Sri Chaitanya"}
+      total={294}
+      goal={4}/>
+    </div>
+  );
+}
+
+export default App;
